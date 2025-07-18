1c9f585a991b9fd438cb2ebbd5645c3fe50d48e3
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2025-07-18 11:19:58 +0200

    feat: get informations of the files

--- script.py
+++ script.py
@@ -1,8 +1,14 @@
 import requests
+import os
 
-message = 'This is a test message.'
-response = requests.post(
-    'http://localhost:8084/index.php',
-    json={'message': message}
-)
-print(response.json())
+files = os.listdir('/home/tod/Bureau/test')
+for file in files:
+    info = os.stat(f'/home/tod/Bureau/test/{file}')
+    print(info)
+
+#message = 'This is a test message.'
+#response = requests.post(
+#    'http://localhost:8084/index.php',
+#    json={'message': message}
+#)
+#print(response.json())
