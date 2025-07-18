6f4d732264ce9108b7fa13243105d370bd14aa95
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2025-07-18 14:03:55 +0200

    feat: add hash to file informations

--- script.py
+++ script.py
@@ -2,12 +2,20 @@ import requests
 import os
 from dotenv import load_dotenv
 import time
+import hashlib
 
 load_dotenv()
 
 API_URL = os.getenv('TRACKBOX_API_URL')
 FOLDER_PATH = os.getenv('TRACKBOX_FOLDER_PATH')
 ACCEPTED_EXTENSIONS = ['.mp3', '.wav']
+
+def compute_file_hash(file_path):
+    hash_sha256 = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
 
 files_infos = []
 
@@ -27,7 +35,8 @@ for (root, dirs, files) in os.walk(FOLDER_PATH):
             'path': relative_path,
             'size_bytes': stat.st_size,
             'modified_ts': stat.st_mtime,
-            'modified_iso': time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(stat.st_mtime)),  # ISO forma
+            'modified_iso': time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(stat.st_mtime)),
+            'hash': compute_file_hash(absolute_path)
         }
 
         files_infos.append(file_info)
@@ -39,4 +48,3 @@ response = requests.post(
     API_URL,
     json={'message': message}
 )
-#print(response.json())
